b03e0126ef595c9352106f374a8f5b374a72c38b
Guilherme <guilherme.gl1997@gmail.com>
2020-10-13 14:24:38 -0300

    feat: views

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,14 +1,17 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
+import orphanagesView from '../views/orphanages_view';
 
 export default {
   async index(request: Request, response: Response) {
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanages = await orphanagesRepository.find();
-
-    return response.json(orphanages);
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images']
+    });
+    
+    return response.json(orphanagesView.renderMany(orphanages));
   },
 
   async show(request: Request, response: Response) {
@@ -16,9 +19,11 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanage = await orphanagesRepository.findOneOrFail(id);
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images']
+    });
 
-    return response.json(orphanage);
+    return response.json(orphanagesView.render(orphanage));
   },
 
   async create(request: Request, response: Response) {
