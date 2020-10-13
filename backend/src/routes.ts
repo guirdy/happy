7995ccab3cc7783903ad970dc41f2cd4d084acfc
Guilherme <guilherme.gl1997@gmail.com>
2020-10-13 12:09:39 -0300

    feat: orphanages detail

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,6 +5,8 @@ const routes = Router();
 
 routes.get('/orphanages', OrphanagesController.index);
 
+routes.get('/orphanages/:id', OrphanagesController.show);
+
 routes.post('/orphanages', OrphanagesController.create);
 
 export default routes;
